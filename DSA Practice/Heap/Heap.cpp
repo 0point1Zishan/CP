1ed827421ec9528944f53339aa595b12cb754216
0PointOne <zishanabder@gmail.com>
2025-01-31 12:34:22 +0600

    Heap

---
+++ DSA Practice/Heap/Heap.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+#define int long long int
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+class heap{
+    public:
+        vector<int> v;
+    heap(){
+        v.push_back(-1);
+    }
+
+    void insert(int val){
+        v.push_back(val);
+
+        int sz = v.size()-1;
+        while(sz > 1 && v[sz/2] < v[sz]){
+            swap(v[sz/2], v[sz]);
+            sz /= 2;
+        }
+    }
+    void print(){
+        for(int i = 1; i < v.size(); i++) cout << v[i] << " ";
+        cout << "\n";
+    }
+};
+
+void solve(){
+    heap a;
+    int n;  cin >> n;
+    for(int i = 0; i < n; i++){
+        int x;  cin >> x;
+        a.insert(x);
+    }
+
+    a.print();
+}
+
+signed main(){
+    fast;
+    int t = 1;
+    // cin >> t;
+    while(t--){
+        solve();
+    }
+}
