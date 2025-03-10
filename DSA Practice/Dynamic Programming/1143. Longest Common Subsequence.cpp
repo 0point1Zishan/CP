3d1e4eb453a08085834bc667a89b8c50702a54a2
0PointOne <zishanabder@gmail.com>
2025-03-10 16:29:40 +0600

    DP on Strings

---
+++ DSA Practice/Dynamic Programming/1143. Longest Common Subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    public:
+        int longestCommonSubsequence(string s, string t) {
+            int n = s.size(), m = t.size();
+            vector<int> pre(m+1, 0), cur(m+1, 0);
+    
+            for(int i = 1; i <= n; i++){
+                for(int j = 1; j <= m; j++){
+                    if(s[i-1] == t[j-1]) cur[j] = 1 + pre[j-1];
+                    else cur[j] = max(pre[j], cur[j-1]);
+                }
+                pre = cur;
+            }
+            return pre[m];
+        }
+    };
